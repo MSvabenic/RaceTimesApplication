44988d435fbd19661fa9536930a5931a667c4e01
Marko <marko.svabenic1@gmail.com>
2021-01-01 13:22:06 +0100

    Adding model and migration
    Adding model and migration

---
+++ Data/Migrations/20210101121930_AddingModels.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RaceTimesApplication.Data.Migrations
+{
+    public partial class AddingModels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserTimes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: false),
+                    LastName = table.Column<string>(nullable: false),
+                    Time = table.Column<double>(nullable: false),
+                    IsApproved = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserTimes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserTimes");
+        }
+    }
+}
